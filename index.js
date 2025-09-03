05dd2532c5401085062d962d34aca75aa83e09ce
boatpremiumcomercial-debug <boatpremiumcomercial@gmail.com>
2025-09-03 15:38:01 -0300

    create index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT; // <- sem fallback
 
 app.get('/boats', async (req, res) => {
   try {
@@ -11,7 +11,10 @@ app.get('/boats', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(boats);
   } catch (error) {
-    res.status(500).json({ error: 'Erro ao buscar dados do ZeroSheets', details: error.message });
+    res.status(500).json({
+      error: 'Erro ao buscar dados do ZeroSheets',
+      details: error.message,
+    });
   }
 });
 
